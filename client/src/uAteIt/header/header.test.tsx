1f430f2f6056ce78cedd8fe4933314b556807573
Chen Friedman <chen.friedman11@gmail.com>
2021-01-10 23:00:46 +0200

    testing added

---
+++ client/src/uAteIt/header/header.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { cleanup} from "@testing-library/react"
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import header from './header';
+
+Enzyme.configure({ adapter: new Adapter() })
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<header/>, div)
+})
+
+it ("shouldn't show user name", () => {
+  const userName = 'chen'
+  expect(userName).not.toBe('')
+})
+
+it ("should show user name", () => {
+  const userName = ''
+  expect(userName).toBe('')
+})
+
+it ("should show user details", () => {
+  const wrapper = shallow(<header/>) 
+  const text = wrapper.find('span')
+  expect(text).not.toBe('')
+})
